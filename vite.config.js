3bed5cf32f9be2cd7f8db14a0c3511b4264f3f92
Pedro Paulo Brasilino <programacaodepedropaulo@gmail.com>
2025-07-14 17:44:05 -0300

    fix: adiciona base correta no Vite e renomeia pasta

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Aquarius-calcados-VR/", // <-- nome EXATO do seu repositório
   plugins: [react()],
-})
+});
