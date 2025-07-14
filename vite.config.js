419926219e75153b07f4652cbaf1c4c9a761bb19
Pedro Paulo Brasilino <programacaodepedropaulo@gmail.com>
2025-07-14 17:45:25 -0300

    fix: config vite base para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Aquarius-calcados-VR/", // <-- nome EXATO do seu repositório
+  base: "/Aquarius-calcados-VR/",
   plugins: [react()],
 });
